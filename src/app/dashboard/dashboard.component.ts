c2fefc5c958c64b73e42ddc7924485f2ff0bef0d
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-05-06 22:44:22 -0300

    aaaa

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { VehicleSelectorComponent } from './vehicle-selector/vehicle-selector.component';
+import { InfoCardsComponent } from './info-cards/info-cards.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+      VehicleSelectorComponent,
+      InfoCardsComponent
+    ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
---
+++ src/app/dashboard/tabble/tabble.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabble',
+  imports: [],
+  templateUrl: './tabble.component.html',
+  styleUrl: './tabble.component.css'
+})
+export class TabbleComponent {
+
+}
