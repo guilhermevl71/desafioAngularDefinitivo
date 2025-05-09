9e063776afd4fc247186cacf05a9b0218348fcd5
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-05-09 01:25:08 -0300

    dashboard done

--- src/app/dashboard/tabble/tabble.component.ts
+++ src/app/dashboard/tabble/tabble.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Dados } from '../../../../models/dados.model';
+import { VeiculosService } from '../../veiculos.service';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tabble',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './tabble.component.html',
   styleUrl: './tabble.component.css'
 })
 export class TabbleComponent {
+  vin = '';
+  dadosVeiculo?: Dados;
+  erro: string = '';
 
+  constructor(private veiculosService: VeiculosService) { }
+
+  buscarDadosPorVin() {
+    this.veiculosService.getDadosPorVin(this.vin).subscribe({
+      next: (dados: Dados) => {
+        this.dadosVeiculo = dados;
+        console.log(dados);
+        this.erro = '';
+      },
+      error: (err: any) => {
+        this.dadosVeiculo = undefined;
+        this.erro = err.error.message || 'Erro ao buscar dados';
+      }
+    });
+  }
+  
 }
--- src/app/veiculos.service.ts
+++ src/app/veiculos.service.ts
@@ -2,16 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Veiculos, VeiculosAPI } from '../../models/veiculo.model';
+import { Dados } from '../../models/dados.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VeiculosService {
   private readonly API = 'http://localhost:3001/vehicles'
+  private readonly API2 = 'http://localhost:3001/vehicleData'
 
   constructor(private http: HttpClient) { }
    
   getVehicles(): Observable<VeiculosAPI> {
     return this.http.get<VeiculosAPI>(this.API);
+  }
+
+  getDadosPorVin(vin: string): Observable<Dados> {
+    return this.http.post<Dados>(this.API2, { vin });
   }
 }
