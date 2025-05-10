3434217f5518fcc72017e302735d725686ac5505
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-05-10 04:09:06 -0300

    login funcionando

---
+++ src/app/login.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private API = 'http://localhost:3001/login'
+
+  constructor(private http: HttpClient) { }
+
+  getlogin(nome: string, senha: string): Observable<Usuario> {
+    return this.http.post<Usuario>(this.API, { nome, senha });
+  }
+}
--- src/app/login/formulario-login/formulario-login.component.ts
+++ src/app/login/formulario-login/formulario-login.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../../../../models/usuario.model';
+import { LoginService } from '../../login.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-login',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './formulario-login.component.html',
   styleUrl: './formulario-login.component.css'
 })
 export class FormularioLoginComponent {
+  nome:string = '';
+  senha:string = '';
+  erro:string = ''
 
+  constructor(private loginservice:LoginService, private router:Router) { }
+
+  teste(){
+    this.loginservice.getlogin(this.nome, this.senha).subscribe({
+      next: (res) => {
+        this.router.navigate(['/home']);
+        this.erro = '';
+      },
+      error: (err: any) => {
+        this.erro = err.error.message;
+      }
+    });
+  }
 }
