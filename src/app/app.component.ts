a15c25980cb2392f1aee02f72783093667ade1c1
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-05-18 02:27:24 -0300

    rodape adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RodapeComponent } from "./rodape/rodape.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/rodape/rodape.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-rodape',
+  imports: [RouterLink],
+  templateUrl: './rodape.component.html',
+  styleUrl: './rodape.component.css'
+})
+export class RodapeComponent {
+
+}
