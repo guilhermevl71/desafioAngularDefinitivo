0d96f1a2311a2931159a612c1f913568bac8f08f
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-05-19 02:30:50 -0300

    responsividade do carrosel

--- src/app/carrosel/carrosel.component.ts
+++ src/app/carrosel/carrosel.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MenuComponent } from "../menu/menu.component";
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-carrosel',
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './carrosel.component.html',
   styleUrl: './carrosel.component.css'
 })
