e1de02609292a6f805b27082ed0a7553dc3d75d8
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-05-20 00:21:52 -0300

    correcoes em lgpd e politica

--- src/app/lgpd/lgpd.component.ts
+++ src/app/lgpd/lgpd.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-lgpd',
-  imports: [RouterLink],
+  imports: [RouterLink, CabecalhoComponent],
   templateUrl: './lgpd.component.html',
   styleUrl: './lgpd.component.css'
 })
--- src/app/politica-de-privacidade/politica-de-privacidade.component.ts
+++ src/app/politica-de-privacidade/politica-de-privacidade.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CabecalhoComponent } from '../cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-politica-de-privacidade',
-  imports: [RouterLink],
+  imports: [RouterLink, CabecalhoComponent],
   templateUrl: './politica-de-privacidade.component.html',
   styleUrl: './politica-de-privacidade.component.css'
 })
